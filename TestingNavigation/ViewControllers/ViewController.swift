3d8a894d2be96f57824a1e43830a37f5915516c6
Zahid Hasan <megamind.d.evilll@gmail.com>
2024-06-27 15:55:59 +0600

    Testing Code based modal presentation

--- TestingNavigation/ViewControllers/ViewController.swift
+++ TestingNavigation/ViewControllers/ViewController.swift
@@ -9,6 +9,10 @@ class ViewController: UIViewController {
     @IBOutlet private(set) var seguePushButton: UIButton!
     @IBOutlet private(set) var segueModalButton: UIButton!
 
+    deinit{
+        print(">>>> ViewController.deinit")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
--- TestingNavigationTests/ViewControllerTests.swift
+++ TestingNavigationTests/ViewControllerTests.swift
@@ -6,7 +6,9 @@
 //
 
 import XCTest
+import ViewControllerPresentationSpy
 @testable import TestingNavigation
+
 final class ViewControllerTests: XCTestCase {
     
     func test_tappingCodePushButton_shouldPushCodeNextViewController(){
@@ -20,18 +22,56 @@ final class ViewControllerTests: XCTestCase {
         executeRunLoop()
         XCTAssertEqual(navigationController.viewControllers.count, 2, "navigation stack")
         let pushedVC = navigationController.viewControllers.last
-        
-//        XCTAssertTrue(pushedVC is CodeNextViewController,
-//        "Expected CodeNextViewController, "
-//        + "but was \(String(describing: pushedVC))")
-        
-        //Or
         guard let codeNextVC = pushedVC as? CodeNextViewController else{
             XCTFail("Expected CodeNextViewController, "
             + "but was \(String(describing: pushedVC))")
             return
         }
         XCTAssertEqual(codeNextVC.label.text, "Pushed From Code")
+    }
+    
+    
+    /// This is a **Not Recommended** way to test a **modally presentedViewController,**
+    /// because the deInit() methods from viewController and CodeNextViewController won't get called,
+    /// which is violating the clean room goals
+    /*
+     
+     func test_INCORRECT_tappingCodeModalButton_shouldPresentCodeNextViewController(){
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let sut: ViewController = storyboard.instantiateViewController(identifier: String(describing: ViewController.self))
+        sut.loadViewIfNeeded()
+        
+        UIApplication.shared.windows.first?.rootViewController = sut
+        
+        tap(sut.codeModalButton)
+        
+        let presentedVC = sut.presentedViewController
+        guard let codeNextVC = presentedVC as? CodeNextViewController else{
+            XCTFail(
+                "Expexted CodeNextViewController. " +
+                "but was \(String(describing: presentedVC))")
+            return
+        }
+        XCTAssertEqual(codeNextVC.label.text, "Modal from code")
+        
+    }
+     
+     */
+    
+    @MainActor func test_tappingCodeModalButton_shouldPresentCodeNextViewController(){
+        let presentationVerifier = PresentationVerifier()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let sut: ViewController = storyboard.instantiateViewController(identifier: String(describing: ViewController.self))
+        sut.loadViewIfNeeded()
+        
+        tap(sut.codeModalButton)
+        
+        let codeNextVC: CodeNextViewController? = presentationVerifier.verify(animated: true, presentingViewController: sut)
+        
+       // XCTAssertNotNil(codeNextVC)
+       
+        XCTAssertEqual(codeNextVC?.label.text, "Modal from code")
+
     }
 
 }
